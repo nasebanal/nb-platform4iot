63a401eea70d2b4c97b0716930c996478c60fdfb
Shunjiro Yatsuzuka <syatsuzuka@oss-erp.jp>
2015-09-27 11:54:19 -0700

    convert code

--- app/controllers/workorders_controller.rb
+++ app/controllers/workorders_controller.rb
@@ -1,11 +1,11 @@
 class WorkordersController < ApplicationController
   before_action :set_workorder, only: [:show, :edit, :update, :destroy]
-	helper_method :get_class
+	helper_method :get_class, :get_status, :get_proc_mode
 
   # GET /workorders
   # GET /workorders.json
   def index
-    @workorders = Workorder.order(:id)
+    @workorders = Workorder.order("id DESC")
   end
 
   # GET /workorders/1
@@ -25,7 +25,11 @@ class WorkordersController < ApplicationController
   # POST /workorders
   # POST /workorders.json
   def create
-		attr = params.require(:workorder).permit(:proc_mode)
+
+		user = User.find(session[:user_id])
+		params[:workorder][:creator] = user[:name]
+		attr = params.require(:workorder).permit(:proc_mode, :creator)
+
 		@workorder = Workorder.new(attr)
 
     respond_to do |format|
@@ -67,10 +71,41 @@ class WorkordersController < ApplicationController
 
 		def get_class (status)
 
-			if status == 1 then
-				return "success"
-			elsif status == 2 then
-				return "danger"
+			case status
+
+				when 1 then
+					return "success"
+				when 2 then
+					return "danger"
+
+			end
+		end
+
+		def get_proc_mode (proc_mode)
+
+			case proc_mode
+
+				when 0 then
+					return "L0"
+				when 1 then
+					return "L1"
+				when 2 then
+					return "L2"
+
+			end
+		end
+
+		def get_status (status)
+
+			case status
+
+				when 0 then
+					return "WAITING"
+				when 1 then
+					return "SUCCESS"
+				when 2 then
+					return "ERROR"
+
 			end
 		end
 
